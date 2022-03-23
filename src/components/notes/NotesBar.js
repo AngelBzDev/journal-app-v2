4452775e69d7e9c49df449ab5ae3b545284145e0
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-23 17:21:56 -0600

    Componente que muestra la fecha y botones para cambiar la imagen y guardar la nota

---
+++ src/components/notes/NotesBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NotesBar = () => {
+  return (
+    <div className='flex justify-between px-6 py-4 text-sm font-semibold border-b-2'>
+      <p>01/01/2022</p>
+      <div className='flex'>
+        <button className='mr-6 font-semibold hover:text-strongGreen'>Imagen</button>
+        <button className='font-semibold hover:text-strongGreen'>Guardar</button>
+      </div>
+    </div>
+  )
+}
+
+export default NotesBar
