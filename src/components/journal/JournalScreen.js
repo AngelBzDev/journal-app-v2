7754fb8ef76c648c3d7b36e60e6fe6bacc2fd37e
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-23 17:20:30 -0600

    Pagina de inicio

---
+++ src/components/journal/JournalScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import NotesScreen from "../notes/NotesScreen";
+import JournalEntries from "./JournalEntries";
+/* import NothingSelected from './NothingSelected' */
+import Sidebar from "./Sidebar";
+
+const JournalScreen = () => {
+  return (
+    <>
+      <Sidebar />
+      <div className="block grid-cols-4 gap-6 md:grid vh-80 scroll">
+        <div className="order-first col-start-1 col-end-3 mb-8 border-2 rounded-2xl">
+          {/* <NothingSelected /> */}
+          <NotesScreen />
+        </div>
+        <img
+          src="https://play-lh.googleusercontent.com/40I0MAr1getFF-dLOzxekLi2_C3MC_u0HL2gmqNbXNWy32n9Bo3XIz2YYlAutfCT8aw"
+          alt="Imagen"
+          className="w-2/4 mx-auto mb-8 rounded-2xl md:w-full"
+        />
+        <JournalEntries />
+      </div>
+    </>
+  );
+};
+
+export default JournalScreen;
