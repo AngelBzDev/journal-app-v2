31835728b0f1ebb0300830d88e3e169bd6d879c1
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-23 17:18:01 -0600

    Configuracion de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6531C4",
+        primaryTransparent: "#F1EFF9",
+        green: "#94FFB9",
+        strongGreen: "#57CBB1",
+      },
+      fontFamily: {
+        'montse': ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
