d02e6b9b8209757fada0f8fe72086375ded92fec
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-24 21:40:01 -0600

    Agregando mas props

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ label, name, type }) => {
+const Input = ({ label, name, type, onChange, value }) => {
   return (
     <div className="mb-3">
       <label className="block mb-1 font-semibold text-strongGreen">
@@ -10,6 +10,8 @@ const Input = ({ label, name, type }) => {
         <textarea
           className="w-full px-0 py-2 text-lg font-semibold border-b-2 resize-none vh-50 focus:outline-none focus:border-primary"
           name={name}
+          value={value}
+          onChange={onChange}
         >
           hay mi pichula
         </textarea>
@@ -18,6 +20,8 @@ const Input = ({ label, name, type }) => {
           type={type}
           className="w-full px-0 py-2 text-lg font-semibold border-b-2 focus:outline-none focus:border-primary"
           name={name}
+          value={value}
+          onChange={onChange}
         />
       )}
     </div>
