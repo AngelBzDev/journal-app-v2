a696f4b5ef01d3c2408096c87c29e8f5b84d3087
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-24 21:47:36 -0600

    implementacion del hook useForm

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -5,8 +5,21 @@ import Button from "../ui/Button";
 import RoundedButton from "../ui/RoundedButton";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
 
 const LoginScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <div className="px-5 mx-auto my-10 rounded-2xl md:border-2 md:border-solid md:w-2/6 md:p-8">
       <h2 className="text-3xl font-semibold">Bienvenid@</h2>
@@ -14,9 +27,21 @@ const LoginScreen = () => {
         Inicia para continuar
       </h3>
       <img src={svg} alt="Notas" className="p-10 mx-auto w-80" />
-      <form className="">
-        <Input name="email" type="email" label="Correo electronico" />
-        <Input name="password" type="password" label="Contraseña" />
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          type="email"
+          label="Correo electronico"
+          value={email}
+          onChange={handleInputChange}
+        />
+        <Input
+          name="password"
+          type="password"
+          label="Contraseña"
+          value={password}
+          onChange={handleInputChange}
+        />
         <Button color="bg-primary" textColor="text-white">
           Iniciar
         </Button>
@@ -33,7 +58,12 @@ const LoginScreen = () => {
         <p className="text-sm font-semibold text-gray-400">
           No tienes una cuenta,{" "}
         </p>
-        <Link to={"/auth/register"} className="font-semibold underline underline-offset-4 text-strongGreen">Registrate</Link>
+        <Link
+          to={"/auth/register"}
+          className="font-semibold underline underline-offset-4 text-strongGreen"
+        >
+          Registrate
+        </Link>
       </div>
     </div>
   );
