503e601b63d0f03156a2ff9d9347a83d3232b0ee
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-28 22:26:57 -0600

    configuracion de firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhJHtruUen2gfb-Bc2sjnLTC2HqbDA9FQ",
+  authDomain: "journal-app-react-424ec.firebaseapp.com",
+  projectId: "journal-app-react-424ec",
+  storageBucket: "journal-app-react-424ec.appspot.com",
+  messagingSenderId: "242987171369",
+  appId: "1:242987171369:web:714574cdd235654c99e638"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const auth = getAuth()
+
+export {
+  app,
+  provider,
+  auth
+}
