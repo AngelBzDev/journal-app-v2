d7c7776d9baf7e5ce9fe050862064fe1fe013282
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-28 22:26:35 -0600

    implementacion de thunk que es un middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,23 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import authReducer from "../reducers/authReducer";
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 //Adjunta los reducers para poder usarlos en el createStore
 const reducers = combineReducers({
   //La estructura de este objeto es como se vera el state
   auth: authReducer,
-})
+});
 
 //Para crear un store usamos createStore al cual se le pasa SOLO un reducer como parametro
 export const store = createStore(
   reducers,
-  //Redux DevTools
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  //Esto nos permite trabajar con acciones asincronas
+  composeEnhancers(applyMiddleware(thunk))
+);
 //Despues lo importamos en el punto mas alto de la aplicacion
