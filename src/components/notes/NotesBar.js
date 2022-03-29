68367e9c35bddce0de47f443309adcb24a90e6d8
AngelBzDev <angelbaez.dev@gmail.com>
2022-03-28 22:27:44 -0600

    Agregando estilos

--- src/components/notes/NotesBar.js
+++ src/components/notes/NotesBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotesBar = () => {
   return (
-    <div className='flex justify-between px-6 py-4 text-sm font-semibold border-b-2'>
+    <div className='flex justify-between px-6 py-4 text-sm font-semibold border-b-2 bg-primaryTransparent rounded-t-2xl'>
       <p>01/01/2022</p>
       <div className='flex'>
         <button className='mr-6 font-semibold hover:text-strongGreen'>Imagen</button>
