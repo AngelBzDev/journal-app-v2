7534dba57a45ad8969ac6a1a034b0d83a1eaef1e
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-02 23:29:04 -0600

    Opcion para deshabilitar el boton

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Button = ({ children, color, textColor }) => {
+const Button = ({ children, color, textColor, disabled}) => {
   return (
     <button
-      className={`py-3 px-4 w-full rounded-2xl ${color} text-lg font-semibold ${textColor} my-8`}
+      className={`py-3 px-4 w-full rounded-xl ${color} text-lg font-semibold ${textColor} my-8 disabled:opacity-75`}
+      disabled={disabled}
     >
       {children}
     </button>
