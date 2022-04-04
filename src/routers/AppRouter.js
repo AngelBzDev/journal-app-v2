884766436aac6c388a1d3641061e4cf1d3aa620a
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-03 23:03:58 -0500

    Recordar el usuario que inicio sesion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import JournalScreen from '../components/journal/JournalScreen'
-import AuthRouter from './AuthRouter'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { login } from "../actions/auth";
+import JournalScreen from "../components/journal/JournalScreen";
+import { auth } from "../firebase/firebase.config";
+import AuthRouter from "./AuthRouter";
 
 const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  //Mantener la sesion iniciada
+  useEffect(() => {
+    //Observable que esta al pendiente de la autenticacion, cada que la auth cambie se va a ejecutar
+    auth.onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return (
-    <div className='w-11/12 mx-auto max-w-7xl font-montse'>
+    <div className="w-11/12 mx-auto max-w-7xl font-montse">
       <BrowserRouter>
         <Routes>
-          <Route path='/auth/*' element={<AuthRouter />}/>
-          <Route path='/' element={<JournalScreen />}/>
-          <Route path='*' element={<Navigate to='/auth/login' />}/>
+          <Route path="/auth/*" element={<AuthRouter />} />
+          <Route path="/" element={<JournalScreen />} />
+          <Route path="*" element={<Navigate to="/auth/login" />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
