97418d415e8ebd0375d171d1974aa651d9ccfad6
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-04 00:17:12 -0500

    Spinner Componente

---
+++ src/components/ui/LoadingScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LoadingScreen = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="loadingio-spinner-interwind-jsvbesed1f">
+        <div className="ldio-zo0n7teyyrf">
+          <div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
