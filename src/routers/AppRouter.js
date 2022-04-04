7c5e56c98867e219c5300da64c1332a819aba01d
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-04 00:18:04 -0500

    Funcionalidad para cargar la pagina actual

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { login } from "../actions/auth";
 import JournalScreen from "../components/journal/JournalScreen";
 import { auth } from "../firebase/firebase.config";
 import AuthRouter from "./AuthRouter";
+import LoadingScreen from '../components/ui/LoadingScreen'
 
 const AppRouter = () => {
   const dispatch = useDispatch();
+  //Revisa si esta autenticado o no
+  const [check, setCheck] = useState(true)
+  const [isLoggin, setIsLoggin] = useState(false)
 
   //Mantener la sesion iniciada
   useEffect(() => {
@@ -15,9 +19,18 @@ const AppRouter = () => {
     auth.onAuthStateChanged((user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
+        setIsLoggin(true)
       }
+      else {
+        setIsLoggin(false)
+      }
+      setTimeout(() => {
+        setCheck(false)
+      }, 1000);
     });
-  }, [dispatch]);
+  }, [dispatch, check, isLoggin]);
+
+  if(check) return <LoadingScreen />
 
   return (
     <div className="w-11/12 mx-auto max-w-7xl font-montse">
