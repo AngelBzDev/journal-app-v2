18caba43fd1b3bfdea8a637d7becc0a8ad009a37
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-04 00:16:24 -0500

    Funcionalidad de logout agregada

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { ImExit } from "react-icons/im";
+import { useDispatch } from "react-redux";
+import { startLogout } from "../../actions/auth";
 
 const Sidebar = () => {
+
+  const dispatch = useDispatch()
+
+  const handleLogout = () => {
+    dispatch(startLogout())
+  }
+
   return (
     <div className="flex items-center justify-between px-5 pb-4 my-10 md:px-0">
       <h3 className="text-2xl font-semibold">
         Bienvenid@ <span className="text-primary">Ángel</span>
       </h3>
-      <Link
-        to={"/"}
+      <button
         className="hidden text-lg font-semibold underline md:inline underline-offset-2 text-strongGreen"
+        onClick={handleLogout}
       >
         Cerrar sesión
-      </Link>
-      <Link
+      </button>
+      <button
         to={"/"}
         className="block text-xl font-semibold md:hidden text-strongGreen"
       >
         <ImExit />
-      </Link>
+      </button>
     </div>
   );
 };
