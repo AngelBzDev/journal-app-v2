5cff41da40858699943511448054b27c8e4aea08
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-05 00:33:38 -0500

    Mostrar el nombre de usuario en el sidebar

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { ImExit } from "react-icons/im";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { startLogout } from "../../actions/auth";
 
 const Sidebar = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.auth);
 
   const handleLogout = () => {
-    dispatch(startLogout())
-  }
+    dispatch(startLogout());
+  };
 
   return (
     <div className="flex items-center justify-between px-5 pb-4 my-10 md:px-0">
       <h3 className="text-2xl font-semibold">
-        Bienvenid@ <span className="text-primary">Ángel</span>
+        Bienvenid@ <span className="text-primary">{name}</span>
       </h3>
       <button
         className="hidden text-lg font-semibold underline md:inline underline-offset-2 text-strongGreen"
