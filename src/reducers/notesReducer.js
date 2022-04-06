410df85f6e133af7477c1979d21ecc7814dad446
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-05 23:44:00 -0500

    reducer de las notas

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,24 @@
+/* {
+  notes: [],
+  active: null || {
+    id: id de la nota
+    title: '',
+    body: '',
+    image: '',
+    date: 475334574845
+  }
+} */
+
+const init = {
+  notes: [],
+  active: null
+}
+
+const notesReducer = (state = init, action) => {
+  switch (action.type) {
+
+    default: return state
+  }
+}
+
+export default notesReducer
