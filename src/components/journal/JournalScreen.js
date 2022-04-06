315162902839a394f05e1238b8f3acf9313b2b92
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-05 23:43:08 -0500

    Saber si hay notas activas

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import NotesScreen from "../notes/NotesScreen";
 import JournalEntries from "./JournalEntries";
-/* import NothingSelected from './NothingSelected' */
+import NothingSelected from "./NothingSelected";
 import Sidebar from "./Sidebar";
 
 const JournalScreen = () => {
+  const { notes } = useSelector((state) => state);
+
   return (
     <>
       <Sidebar />
       <div className="block grid-cols-4 gap-6 md:grid vh-80 scroll">
         <div className="order-first col-start-1 col-end-3 mb-8 border border-primary rounded-2xl">
-          {/* <NothingSelected /> */}
-          <NotesScreen />
+          {notes.active ? <NotesScreen /> : <NothingSelected />}
         </div>
         <img
           src="https://play-lh.googleusercontent.com/40I0MAr1getFF-dLOzxekLi2_C3MC_u0HL2gmqNbXNWy32n9Bo3XIz2YYlAutfCT8aw"
