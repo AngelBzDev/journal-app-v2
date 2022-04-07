1798cab172c50044fb412fa6d7e4e32c29038b29
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-06 23:29:35 -0500

    Boton para añadir notas

---
+++ src/components/notes/AddNote.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../ui/Button'
+
+const AddNote = () => {
+  return (
+    <div className=''>
+      <Button color={'bg-primary'}>
+        Agregar
+      </Button>
+    </div>
+  )
+}
+
+export default AddNote
