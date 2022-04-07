282749eafd69fcbb3ca35e11d5bbe33fa7a2b672
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-06 23:31:13 -0500

    reducer de las notas

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -9,16 +9,30 @@
   }
 } */
 
+import types from "../types/types";
+
 const init = {
   notes: [],
-  active: null
-}
+  active: null,
+};
 
 const notesReducer = (state = init, action) => {
   switch (action.type) {
-
-    default: return state
+    case types.notesActive:
+      return {
+        ...state,
+        active: {
+          ...action.payload,
+        },
+      };
+    case types.notesLoad:
+      return {
+        ...state,
+        notes: [...action.payload],
+      };
+    default:
+      return state;
   }
-}
+};
 
-export default notesReducer
+export default notesReducer;
