2786c07540aab96e8f0616595fe832e7ced9425f
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-06 23:22:51 -0500

    Acciones de las notas

---
+++ src/actions/notes.js
@@ -0,0 +1,37 @@
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase/firebase.config";
+import types from "../types/types";
+
+export const activeNote = (id, note) => ({
+  type: types.notesActive,
+  payload: {
+    id,
+    ...note,
+  },
+});
+
+export const setNotes = (notes) => ({
+  type: types.notesLoad,
+  payload: notes,
+});
+
+export const startNewNote = () => {
+  return async (dispath, getState) => {
+    const { uid } = getState().auth;
+
+    const newNote = {
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+
+    const { id } = await addDoc(
+      collection(db, `${uid}/journal/notes`),
+      newNote
+    );
+
+    dispath(activeNote(id, newNote));
+  };
+};
+
+
