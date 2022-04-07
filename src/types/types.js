a9f50f93004cadb89e2b840c0cc2035627faae7b
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-06 23:32:41 -0500

    Tipos para las notas

--- src/types/types.js
+++ src/types/types.js
@@ -4,11 +4,19 @@ const types = {
   //Autenticacion
   login: '[Auth] Login',
   logout: '[Auth] Logout',
-//Interfaz
+  //Interfaz
   setError: '[UI] Set Error',
   removeError: '[UI] Remove Error',
   uiStartLoading: '[UI] Start loading',
-  uiFinishLoading: '[UI] Finish loading'
+  uiFinishLoading: '[UI] Finish loading',
+  //Notas
+  notesAddNew: '[Notes] New Note',
+  notesActive: '[Notes] Set active Note',
+  notesLoad: '[Notes] Load notes',
+  notesUpdate: '[Notes] Update note',
+  notesFileUrl: '[Notes] Update image url',
+  notesDelete: '[Notes] Delete note',
+  notesCleaning: '[Notes] Notes Cleaning',
 }
 
 export default types
