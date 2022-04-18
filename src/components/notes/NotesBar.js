c63447691503a42e0004756408df4ff3823d1ead
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:14:02 -0500

    Mostrar la fecha, save note, y seleccionar el archivo

--- src/components/notes/NotesBar.js
+++ src/components/notes/NotesBar.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { startNewNote } from "../../actions/notes";
+import { startNewNote, startSaveNote, startUploadingFile } from "../../actions/notes";
 import ButtonBar from "../ui/ButtonBar";
 import {
   AiOutlineFileAdd as Add,
   AiOutlineFileImage as Image,
   AiOutlineSave as Save,
 } from "react-icons/ai";
+import moment from "moment";
 
 const NotesBar = () => {
   const { notes } = useSelector((state) => state);
   const dispatch = useDispatch();
 
+  const formatDate = moment(notes.active?.date).format("DD-MM-YYYY");
+
   const handleAddNote = () => {
     dispatch(startNewNote());
+  };
+
+  const handleSaveNote = () => {
+    dispatch(startSaveNote(notes.active));
+  };
+
+  const inpImage = useRef();
+  const handleClickImage = () => inpImage.current.click();
+  const handleChangeimage = ({ target }) => {
+    const file = target.files[0];
+    if (file) dispatch(startUploadingFile(file));
   };
 
   return (
@@ -24,12 +38,19 @@ const NotesBar = () => {
       </ButtonBar>
       {notes.active && (
         <div className="flex">
-          <p className="mr-6">01/01/2022</p>
-          <ButtonBar margin={"mr-6"}>
+          <p className="mr-6">{formatDate}</p>
+          <input
+            type="file"
+            className="hidden"
+            ref={inpImage}
+            onChange={handleChangeimage}
+            id="inpImage"
+          />
+          <ButtonBar margin={"mr-6"} onClick={handleClickImage}>
             <p className="hidden md:inline">Imagen</p>
             <Image className="ml-1" size={"1.2rem"} />
           </ButtonBar>
-          <ButtonBar>
+          <ButtonBar onClick={handleSaveNote}>
             <p className="hidden md:inline">Guardar</p>
             <Save className="ml-1" size={"1.2rem"} />
           </ButtonBar>
