5ddb437d2a561d57866d19a31f178e95a4b1631e
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:15:38 -0500

    Nuevas props

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Button = ({ children, color, textColor, disabled}) => {
+const Button = ({ children, color, textColor, disabled, click}) => {
   return (
     <button
       className={`py-3 px-4 w-full rounded-xl ${color} text-lg font-semibold ${textColor} my-8 disabled:opacity-75`}
       disabled={disabled}
+      onClick={click}
     >
       {children}
     </button>
