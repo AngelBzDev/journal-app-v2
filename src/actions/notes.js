5613b36d08eda3e5b0c2f73cf514fd2d3995c45f
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:09:55 -0500

    Acciones para guardar, actualizar la nota y subir la imagen

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,5 +1,7 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc, doc } from "firebase/firestore";
+import Swal from "sweetalert2";
 import { db } from "../firebase/firebase.config";
+import { fileUpload } from "../helpers/fileUpload";
 import loadNotes from "../helpers/loadNotes";
 import types from "../types/types";
 
@@ -16,6 +18,14 @@ export const setNotes = (notes) => ({
   payload: notes,
 });
 
+export const refreshNote = (id, note) => ({
+  type: types.notesUpdate,
+  payload: {
+    id,
+    note,
+  },
+});
+
 export const startNewNote = () => {
   return async (dispath, getState) => {
     const { uid } = getState().auth;
@@ -24,6 +34,7 @@ export const startNewNote = () => {
       title: "",
       body: "",
       date: new Date().getTime(),
+      url: null,
     };
 
     const { id } = await addDoc(
@@ -36,9 +47,44 @@ export const startNewNote = () => {
 };
 
 export const startLoadingNotes = (uid) => {
-  return async(dispath) => {
-    const notes = await loadNotes(uid)
-    dispath(setNotes(notes))
-  }
+  return async (dispath) => {
+    const notes = await loadNotes(uid);
+    dispath(setNotes(notes));
+  };
+};
 
-}
+export const startSaveNote = (note) => {
+  return async (dispath, getState) => {
+    const { uid } = getState().auth;
+
+    const noteToFirestore = { ...note };
+
+    await updateDoc(
+      doc(db, `${uid}/journal/notes/`, `${note.id}`),
+      noteToFirestore
+    );
+
+    dispath(refreshNote(note.id, note));
+
+    Swal.fire("Nice!! 👏🏽", "La nota se guardo con exito", "success");
+  };
+};
+
+export const startUploadingFile = (file) => {
+  return async (dispath, getState) => {
+    const { active: activeNote } = getState().notes;
+
+    Swal.fire({
+      title: "!Awanta 👌",
+      text: "Se esta subiendo la imagen",
+      didOpen: () => Swal.showLoading(),
+    });
+
+    const fileUrl = await fileUpload(file);
+    activeNote.url = fileUrl;
+
+    dispath(startSaveNote(activeNote));
+
+    Swal.close();
+  };
+};
