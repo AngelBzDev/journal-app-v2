a4d8a9ba470065932b51d8fd262b0d36859c1f9e
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:12:19 -0500

    Ajustes en diseño

--- src/components/journal/NothingSelected.js
+++ src/components/journal/NothingSelected.js
@@ -3,7 +3,7 @@ import { AiOutlineStar } from "react-icons/ai";
 
 const NothingSelected = () => {
   return (
-    <div className="flex items-center justify-center h-5/6">
+    <div className="flex items-center justify-center my-10 h-5/6 md:my-0">
       <p className="text-xl font-bold text-center text-strongGreen">
       <AiOutlineStar style={{margin: '0 auto 15px auto', }} size='30px' /> Seleccione alguna nota
       </p>
