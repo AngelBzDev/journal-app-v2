7bafd43514a90488107a80bfa7bb157a2d357ad8
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:10:58 -0500

    Mostrar las notas si hay alguna

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -3,20 +3,21 @@ import { useSelector } from "react-redux";
 import JournalEntry from "./JournalEntry";
 
 const JournalEntries = () => {
-  const state = useSelector(state => state)
-  const {notes} = state.notes
+  const state = useSelector((state) => state);
+  const { notes } = state.notes;
+
   return (
     <>
-      {notes.length > 1 ? (
+      {notes.length > 0 ? (
         <div className="col-start-4 col-end-5 overflow-scroll scroll">
-        {notes.map((nota, i) => (
-          <JournalEntry key={i}/>
-        ))}
-      </div>
+          {notes.map((nota) => (
+            <JournalEntry key={nota.id} {...nota} />
+          ))}
+        </div>
       ) : (
         <div className="col-start-4 col-end-5 text-center">
-            <p className="mb-2 text-2xl font-medium">Aun no tienes notas</p>
-            <p className="text-2xl font-medium">Agrege alguna 👽</p>
+          <p className="mb-2 text-2xl font-medium">Aun no tienes notas</p>
+          <p className="text-2xl font-medium">Agrege alguna 👽</p>
         </div>
       )}
     </>
