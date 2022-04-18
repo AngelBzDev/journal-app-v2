dff81ea4711b82513a19e7015d9d8b800d4da5e1
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:11:39 -0500

    Mostrar la informacion de la nota en su tarjeta

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React from "react";
+import moment from "moment";
+import { useDispatch } from "react-redux";
+import { activeNote } from "../../actions/notes";
 
-const JournalEntry = () => {
+const JournalEntry = ({ title, body, id, url, date }) => {
+  const formatDate = moment(date).format("DD-MM-YYYY");
+  const dispath = useDispatch();
 
-  const imgUrl = 'https://play-lh.googleusercontent.com/40I0MAr1getFF-dLOzxekLi2_C3MC_u0HL2gmqNbXNWy32n9Bo3XIz2YYlAutfCT8aw'
+  const handleClickEntry = () => {
+    dispath(activeNote(id, { title, body, url, date }));
+  };
 
   return (
-    <div className='flex items-center justify-between p-3 mb-6 transition-all duration-300 border cursor-pointer bg-primaryTransparent rounded-2xl hover:bg-gray-100 hover:border-strongGreen'>
-      <img src={imgUrl} alt="Imagen" style={{width: '60px', height: '60px'}} className="rounded-2xl" />
-      <div className='ml-4'>
-        <h4 className='text-lg font-bold text-primary'>Esta es una nota</h4>
-        <p className='mt-2 font-medium'>fsdfkldf hidsfs idsf so fsdfi sdofdif cidgosf</p>
-        <p className='mt-2 text-sm font-bold text-right text-strongGreen'>01/01/2022</p>
+    <div
+      className="flex items-center p-3 mb-6 transition-all duration-300 border shadow-lg cursor-pointer bg-primaryTransparent rounded-2xl hover:bg-gray-100 hover:border-strongGreen"
+      onClick={handleClickEntry}
+    >
+      {url && (
+        <img
+          src={url}
+          alt="Imagen"
+          style={{ width: "60px", height: "60px" }}
+          className="rounded-2xl"
+        />
+      )}
+      <div className="w-full ml-4">
+        <h4 className="text-lg font-bold text-primary">{title}</h4>
+        <p className="block w-full mt-2 font-medium textBody">{body}</p>
+        <p className="mt-2 text-sm font-bold text-right text-strongGreen">
+          {formatDate}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default JournalEntry
+export default JournalEntry;
