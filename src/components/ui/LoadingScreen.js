81e232682482ca5a8ac1d6f2f45589ca795e3c69
AngelBzDev <angelbaez.dev@gmail.com>
2022-04-17 21:15:58 -0500

    Nuevo spinner de carga

--- src/components/ui/LoadingScreen.js
+++ src/components/ui/LoadingScreen.js
@@ -3,24 +3,19 @@ import React from "react";
 const LoadingScreen = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <div className="loadingio-spinner-interwind-jsvbesed1f">
-        <div className="ldio-zo0n7teyyrf">
+      <div className="loadingio-spinner-double-ring-qmtliysjy0j">
+        <div className="ldio-jkyrqgyrq6h">
+          <div></div>
+          <div></div>
           <div>
-            <div>
-              <div>
-                <div></div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div></div>
-              </div>
-            </div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default LoadingScreen;
